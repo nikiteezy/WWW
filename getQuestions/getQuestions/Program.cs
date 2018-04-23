f6b567c87da2e31d9e7980f53796f2d2e9bf0129
nikiteezy <nikiteezy@gmail.com>
2018-04-23 18:10:51 +0300

    fix text bags, remove questions without text

--- getQuestions/getQuestions/Program.cs
+++ getQuestions/getQuestions/Program.cs
@@ -5,38 +5,46 @@ namespace getQuestions
 {
     internal class Program
     {
-        private static HtmlWeb hw = new HtmlWeb();
-        private static HtmlDocument doc = hw.Load(@"https://db.chgk.info/random/answers/complexity1/limit1");
-
         private static void Main(string[] args)
         {
-            string allText, question, answer, setOff, comment, source, author, result, tournament;
+            do
+            {
+                HtmlWeb hw = new HtmlWeb();
+                HtmlDocument doc = hw.Load(@"https://db.chgk.info/random/answers/complexity1/limit1");
 
-            HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id=\"main\"]/div[3]/div");
+                // //*[@id="main"]/div[3]/div/div/div === для раздатки
+                // //*[@id="main"]/div[3]/div/img  === для картинки
+                string allText, question, answer, setOff, comment, source, author, result, tournament, image;
 
-            allText = node.InnerText;
-            node = doc.DocumentNode.SelectSingleNode("//*[@id=\"main\"]/div[3]/div/p[1]");
+                HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id=\"main\"]/div[3]/div/img");
 
-            tournament = node.InnerText;
-            answer = getText(5, 'О');
-            comment = getText(5, 'К');
-            author = getText(5, 'А');
-            result = getText(5, 'Р');
-            source = getText(5, 'И');
-            setOff = getText(5, 'З');
+                try
+                {
+                    image = node.InnerText;
+                }
+                catch
+                {
+                    node = doc.DocumentNode.SelectSingleNode("//*[@id=\"main\"]/div[3]/div");
 
-            question = allText.Replace(tournament, "");
-            question = question.Replace(answer, "");
-            question = question.Replace(comment, "");
-            question = question.Replace(author, "");
-            question = question.Replace(result, "");
-            question = question.Replace(source, "");
-            question = question.Replace(setOff, "");
+                    allText = node.InnerText;
+                    node = doc.DocumentNode.SelectSingleNode("//*[@id=\"main\"]/div[3]/div/p[1]");
 
-            Console.WriteLine(question);
+                    tournament = node.InnerText;
+                    answer = getText(5, 'О', doc);
+                    comment = getText(5, 'К', doc);
+                    author = getText(5, 'А', doc);
+                    result = getText(5, 'Р', doc);
+                    source = getText(5, 'И', doc);
+                    setOff = getText(5, 'З', doc);
+
+                    question = delExtra(allText, answer, setOff, comment, source, author, result, tournament);
+
+                    Console.WriteLine(question);
+                }
+            } while (true);
         }
 
-        private static string getText(int position, char literal)
+        private static string getText(int position, char literal, HtmlDocument doc)
         {
             string temp;
             for (int i = 2; i <= 10; i++)
@@ -56,6 +64,24 @@ namespace getQuestions
                 }
             }
             return "...";
+        }
+
+        private static string delExtra(string allText, string answer, string setOff,
+            string comment, string source, string author, string result, string tournament)
+        {
+            string question;
+
+            question = allText.Replace(tournament, "");
+            question = question.Replace(answer, "");
+            question = question.Replace(comment, "");
+            question = question.Replace(author, "");
+            question = question.Replace(result, "");
+            question = question.Replace(source, "");
+            question = question.Replace(setOff, "");
+            question = question.Replace("&nbsp;", "");
+            question = question.Replace("&mdash", "");
+
+            return question;
         }
     }
 }
